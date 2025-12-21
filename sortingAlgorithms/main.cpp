7a7e0e56b23ae776ba79541b46b6e808b02a20f4
Nastya <olinan106@gmail.com>
2025-12-21 18:47:52 +0400

    add stability test

--- sortingAlgorithms/main.cpp
+++ sortingAlgorithms/main.cpp
@@ -4,6 +4,7 @@
 #include "PerformanceTests.h"
 #include "OperationsAnalysis.h"
 #include "DataTypesAnalysis.h" 
+#include "StabilityTest.h"
 #include "Utils.h"
 #include <algorithm>
 
@@ -46,8 +47,9 @@ int main() {
         cout << "1. Демонстрация работы на ручном вводе\n";
         cout << "2. Запуск тестов производительности (10,000 тестов на размер)\n";
         cout << "3. Анализ количества операций (сравнения и обмены)\n";
-        cout << "4. Сравнение алгоритмов на разных типах данных\n";  
-        cout << "5. Выход\n";
+        cout << "4. Сравнение алгоритмов на разных типах данных\n"; 
+        cout << "5. Тест на стабильность сортировок\n";
+        cout << "6. Выход\n";
         cout << "Выберите пункт: ";
 
         int choice;
@@ -62,10 +64,13 @@ int main() {
         else if (choice == 3) {
             runOperationsAnalysis();
         }
-        else if (choice == 4) {  // Новая опция
+        else if (choice == 4) { 
             runDataTypesAnalysis();
         }
-        else if (choice == 5) {  // Изменен номер выхода
+        else if (choice == 5) {
+            runStabilityTest();
+        }
+        else if (choice == 6) { 
             cout << "Выход из программы.\n";
             break;
         }
