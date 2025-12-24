8aba28d8597c4752a836e2a74cb7a3e9e5ee14bc
Nastya <olinan106@gmail.com>
2025-12-24 12:55:15 +0400

    remove stability and memory test

--- sortingAlgorithms/MemoryTest.h
+++
@@ -1,6 +0,0 @@
-﻿#pragma once
-#include <iostream>
-#include <vector>
-#include <chrono>
-
-void runMemoryUsageTest();
--- sortingAlgorithms/main.cpp
+++ sortingAlgorithms/main.cpp
@@ -4,8 +4,6 @@
 #include "PerformanceTests.h"
 #include "OperationsAnalysis.h"
 #include "DataTypesAnalysis.h" 
-#include "StabilityTest.h"
-#include "MemoryTest.h"
 #include "Utils.h"
 #include <algorithm>
 
@@ -49,9 +47,7 @@ int main() {
         cout << "2. Запуск тестов производительности (10,000 тестов на размер)\n";
         cout << "3. Анализ количества операций (сравнения и обмены)\n";
         cout << "4. Сравнение алгоритмов на разных типах данных\n"; 
-        cout << "5. Тест на стабильность сортировок\n";
-        cout << "6. Тест использования памяти\n";
-        cout << "7. Выход\n";
+        cout << "5. Выход\n";
         cout << "Выберите пункт: ";
 
         int choice;
@@ -69,13 +65,7 @@ int main() {
         else if (choice == 4) { 
             runDataTypesAnalysis();
         }
-        else if (choice == 5) {
-            runStabilityTest();
-        }
-        else if (choice == 6) {
-            runMemoryUsageTest();
-        }
-        else if (choice == 7) { 
+        else if (choice == 5) { 
             cout << "Выход из программы.\n";
             break;
         }
